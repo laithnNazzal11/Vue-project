f345106a8403f9827ae132c83bf83a28d201e9b6
Laith Nazzal <laith.gorge@gmail.com>
2025-06-26 17:10:06 +0300

    first commit

---
+++ pizzas.js
@@ -0,0 +1,69 @@
+// Here's the array of pizzas to use in your project
+// Copy and paste it as you wish
+
+menu: [
+    {
+      id: 1,
+      image: "pizza-1.png",
+      name: "BISMARCK",
+      price: 30,
+      calories: "800 kcal",
+      fats: "50 g",
+    },
+    {
+      id: 2,
+      image: "pizza-2.png",
+      name: "FIORI DI ZUCCA",
+      price: 50,
+      calories: "800 kcal",
+      fats: "50 g",
+    },
+    {
+      id: 3,
+      image: "pizza-3.png",
+      name: "VALDOSTANA",
+      price: 55,
+      calories: "800 kcal",
+      fats: "50 g",
+    },
+    {
+      id: 4,
+      image: "pizza-4.png",
+      name: "PIZZA TARTUFATA",
+      price: 45,
+      calories: "800 kcal",
+      fats: "50 g",
+    },
+    {
+      id: 5,
+      image: "pizza-5.png",
+      name: "FRANCESCANA",
+      price: 25,
+      calories: "800 kcal",
+      fats: "50 g",
+    },
+    {
+      id: 6,
+      image: "pizza-6.png",
+      name: "BOSCAIOLA",
+      price: 85,
+      calories: "800 kcal",
+      fats: "50 g",
+    },
+    {
+      id: 7,
+      image: "pizza-7.png",
+      name: "MARE E MONTI",
+      price: 65,
+      calories: "800 kcal",
+      fats: "50 g",
+    },
+    {
+      id: 8,
+      image: "pizza-8.png",
+      name: "MARE E MONTI",
+      price: 95,
+      calories: "800 kcal",
+      fats: "50 g",
+    },
+  ];
